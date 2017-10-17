7aff3a78806f0dbd6c75b86d9db4a2007de3384b
Christian Schmid <christian.schmid@ntb.ch>
2017-10-17 16:57:50 +0200

    Added GeoFire to project and implemented simple usage example. Added second functionbody for updating the DB.

--- app/src/main/java/mse_mobop/ski_compass/MainActivity.java
+++ app/src/main/java/mse_mobop/ski_compass/MainActivity.java
@@ -2,6 +2,18 @@ package mse_mobop.ski_compass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // test
+
+        final TextView tv = (TextView) findViewById(R.id.textView);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               tv.setText("Hi there");
+
+               // Prepare
+               DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("GeoFire");
+               GeoFire geoFire = new GeoFire(dbRef);
+
+               // Write a location to db
+               geoFire.setLocation("firebase-hq", new GeoLocation(5, 5));
+
+               // Search around a location
+               GeoQuery query = geoFire.queryAtLocation(new GeoLocation(5, 5), 10);
+               query.addGeoQueryEventListener(new GeoQueryEventListener() {
+
+                   // This method is called every time a location gets into the radius...
+                   // should save the list once and then deregister...
+                   @Override
+                   public void onKeyEntered(String key, GeoLocation location) {
+                       Toast.makeText(MainActivity.this, "Something is coming... Key: " + key, Toast.LENGTH_LONG).show();
+                   }
+
+                   @Override
+                   public void onKeyExited(String key) {
+
+                   }
+
+                   @Override
+                   public void onKeyMoved(String key, GeoLocation location) {
+
+                   }
+
+                   @Override
+                   public void onGeoQueryReady() {
+
+                   }
+
+                   @Override
+                   public void onGeoQueryError(DatabaseError error) {
+
+                   }
+               });
+
+
+           }
+        });
     }
 }
--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,11 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.getSkiResorts = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
+});
+
+exports.updateDB = functions.https.onRequest((request, response) => {
+    var test = request.query.key;
+    response.send("Updating DB, key: " + test);
 });
