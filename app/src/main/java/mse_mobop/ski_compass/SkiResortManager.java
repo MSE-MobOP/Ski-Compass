7cf172d50a88d550e91cf79b995782a8adaad834
Martin Messmer <messmermartin@hotmail.com>
2017-10-27 21:35:27 +0200

    Ask for Location Permission. Implemented GeoFire Atombomb to get nearest Skiresorts sorted by Distance Get Skiresort Name from GeoFire Request and Display in list

---
+++ app/src/main/java/mse_mobop/ski_compass/SkiResortManager.java
@@ -0,0 +1,89 @@
+package mse_mobop.ski_compass;
+
+import android.location.LocationManager;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Martin on 27.10.2017.
+ */
+
+public class SkiResortManager {
+
+    private static final int MAX_RESULTS = 10;
+    private static SkiResortManager instance;
+
+    private int numberOfResorts = 0;
+    GeoFire geoFire = new GeoFire(FirebaseDatabase.getInstance().getReference("GeoFire"));
+
+    private SkiResortManager() {
+    }
+
+    public static SkiResortManager getInstance() {
+        if (instance == null) {
+            instance = new SkiResortManager();
+        }
+        return instance;
+    }
+
+    public void loadNearestResorts(GeoLocation location, final ArrayAdapter<String> adapter) {
+        numberOfResorts = 0;
+        final GeoQuery query = geoFire.queryAtLocation(location, 1);
+        query.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                numberOfResorts++;
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("skiresorts/" + key);
+                ref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        adapter.add(dataSnapshot.child("name").getValue(String.class));
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+                // do nothing
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+                // do nothing
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                if (numberOfResorts < MAX_RESULTS) {
+                    query.setRadius(query.getRadius() + 1);
+                }
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+                Log.e(TAG, "onGeoQueryError: " + error);
+            }
+        });
+    }
+}
