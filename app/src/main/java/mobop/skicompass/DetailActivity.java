fd7444e5a04bee55fc776b4db407a4e1cc87652d
Christian Schmid <christian.schmid@ntb.ch>
2017-11-11 15:55:19 +0100

    Added details to detailList in detailActivity.

--- app/src/main/java/mobop/skicompass/DetailActivity.java
+++ app/src/main/java/mobop/skicompass/DetailActivity.java
@@ -14,19 +14,22 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 import java.util.Locale;
+
 import mobop.skicompass.dataarchitecture.OperatingStatus;
 import mobop.skicompass.dataarchitecture.SkiResort;
 
 /**
  * Created by artanpapaj on 26.10.17.
  */
-@java.lang.SuppressWarnings("squid:MaximumInheritanceDepth") // AppCompatActivity has already too much parents... Would be kind of a lot work to make it better
+@java.lang.SuppressWarnings("squid:MaximumInheritanceDepth")
+// AppCompatActivity has already too much parents... Would be kind of a lot work to make it better
 public class DetailActivity extends AppCompatActivity {
 
     private SkiResort selectedResort;
-    private DetailRowData[] rowData;
-    private static final int NUM_ROWS = 2;
+    private List<DetailRowData> rowData;
     private static final String WRONG_BUTTON = "Wrong binding of Button";
 
     @Override
@@ -38,14 +41,18 @@ public class DetailActivity extends AppCompatActivity {
 
         ListView detailList = (ListView) findViewById(R.id.detailList);
         detailList.setEnabled(false);
-        rowData = new DetailRowData[NUM_ROWS];
+
+        DetailRowData[] rowDataArray = new DetailRowData[rowData.size()];
+        rowDataArray = rowData.toArray(rowDataArray);
 
         setTitle(selectedResort.getName());
 
         setWeather();
         setStatusDescription();
+        setLiftsDescritpion();
+        setSlopesDescription();
 
-        DetailArrayAdapter detailArrayAdapter = new DetailArrayAdapter(this, rowData);
+        DetailArrayAdapter detailArrayAdapter = new DetailArrayAdapter(this, rowDataArray);
         detailList.setAdapter(detailArrayAdapter);
 
         checkWebButton();
@@ -60,6 +67,8 @@ public class DetailActivity extends AppCompatActivity {
         setWeatherIcon();
         setWeatherDescription();
         setTemperature();
+        setMinMaxTemperatureDescription();
+        setWindDescription();
     }
 
     /**
@@ -68,7 +77,7 @@ public class DetailActivity extends AppCompatActivity {
     private void setWeatherDescription() {
         String weatherDescriptionText;
 
-        switch (Locale.getDefault().getLanguage()){
+        switch (Locale.getDefault().getLanguage()) {
             case "de":
                 weatherDescriptionText = selectedResort.getWeatherData().getWeather().get(0).getDescriptionDE();
                 break;
@@ -80,7 +89,7 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         // fill detailList
-        rowData[0] = new DetailRowData(getResources().getString(R.string.detailWeather), weatherDescriptionText);
+        rowData.add(new DetailRowData(getResources().getString(R.string.detailWeather), weatherDescriptionText));
     }
 
     /**
@@ -88,8 +97,7 @@ public class DetailActivity extends AppCompatActivity {
      */
     private void setTemperature() {
         TextView textView = (TextView) findViewById(R.id.temperatureTextView);
-        double tempAbs = selectedResort.getWeatherData().getMain().getTemp();
-        double temp = tempAbs - 273.0;
+        double temp = Utils.absToNormal(selectedResort.getWeatherData().getMain().getTemp());
         int tempInt = (int) temp;
         textView.setText(getResources().getString(R.string.detailTemp, tempInt));
     }
@@ -103,8 +111,18 @@ public class DetailActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), getResources().getText(R.string.detailErrorWeatherIcon), Toast.LENGTH_LONG).show();
         }
         ImageView weatherImageView = (ImageView) findViewById(R.id.weatherImageView);
-        int id = getResources().getIdentifier("weather_" + weatherIconName,"drawable", getPackageName());
+        int id = getResources().getIdentifier("weather_" + weatherIconName, "drawable", getPackageName());
         weatherImageView.setImageResource(id);
+    }
+
+    /**
+     * Sets the min and max temperature to the detail description list
+     */
+    private void setMinMaxTemperatureDescription() {
+        double min = Utils.absToNormal(selectedResort.getWeatherData().getMain().getTempMin());
+        double max = Utils.absToNormal(selectedResort.getWeatherData().getMain().getTempMax());
+        String tempMinMax = getResources().getString(R.string.detailTempMinMaxFormat, (int) min, (int) max);
+        rowData.add(new DetailRowData(getResources().getString(R.string.detailTempMinMax), tempMinMax));
     }
 
     /**
@@ -127,7 +145,22 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         // fill detailList
-        rowData[1] = new DetailRowData(getResources().getString(R.string.detailStatus), statusDescription);
+        rowData.add(new DetailRowData(getResources().getString(R.string.detailStatus), statusDescription));
+    }
+
+    private void setWindDescription() {
+        /*double windSpeed = selectedResort.getWeatherData().getWind().getSpeed();
+        rowData.add(new DetailRowData(getResources().getString(R.string.detailWind), Double.toString(windSpeed)));*/
+    }
+
+    private void setLiftsDescritpion() {
+        int openLifts = selectedResort.getOpenedLifts();
+        rowData.add(new DetailRowData(getResources().getString(R.string.detailOpenLifts), Integer.toString(openLifts)));
+    }
+
+    private void setSlopesDescription() {
+        double openSlopes = selectedResort.getOpenedSlops();
+        rowData.add(new DetailRowData(getResources().getString(R.string.detailOpenSlopes), Double.toString(openSlopes)));
     }
 
     /**
@@ -142,6 +175,7 @@ public class DetailActivity extends AppCompatActivity {
 
     /**
      * Creates a web-browser intent, checks if it can be handled and fires it
+     *
      * @param v
      */
     public void createWebIntent(View v) {
@@ -161,6 +195,7 @@ public class DetailActivity extends AppCompatActivity {
 
     /**
      * Creates a google maps intent, checks if it can be handled and fires it
+     *
      * @param v
      */
     public void createMapIntent(View v) {
@@ -179,7 +214,8 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     /**
-     * Resizes the list to fit all items in height
+     * Resize the list to fit all items in height
+     *
      * @param detailList
      */
     private void blowDetailListToFullHeight(ListView detailList) {
@@ -190,7 +226,8 @@ public class DetailActivity extends AppCompatActivity {
         View view = null;
         for (int i = 0; i < detailListAdapter.getCount(); i++) {
             view = detailListAdapter.getView(i, view, detailList);
-            if (i == 0) view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ActionBar.LayoutParams.WRAP_CONTENT));
+            if (i == 0)
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ActionBar.LayoutParams.WRAP_CONTENT));
             view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
             totalHeight += view.getMeasuredHeight();
         }
